3100de34bcec2ed9a26789f3c9c40cf0a2a9b6c5
Vparikh01 <vihaaanparikh@icloud.com>
2025-04-20 00:53:12 -0700

    Initial Commit

---
+++ PingPongReviews/PingPongReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPongReviewsApp.swift
+//  PingPongReviews
+//
+//  Created by vihaan parikh on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PingPongReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PingPongReviewsTests/PingPongReviewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PingPongReviewsTests.swift
+//  PingPongReviewsTests
+//
+//  Created by vihaan parikh on 4/20/25.
+//
+
+import Testing
+@testable import PingPongReviews
+
+struct PingPongReviewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
